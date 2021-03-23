941746929adc6c0d77785e2136931db1b967d23b
DaniilKurandin <kurandin1995@mail.ru>
2021-03-23 16:08:39 +0500

    MASTER-2: add essential_homework1

--- src/essential_homework1/task2/Main.java
+++ src/essential_homework1/task2/Main.java
@@ -1,8 +1,23 @@
 package essential_homework1.task2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Rectangle rectangle = new Rectangle();
-        System.out.println(rectangle.areaCalculator(10, 23));
+
+        System.out.println("Select an operation: \"1\" - rectangle area, \"2\" - rectangle perimeter");
+        while (true)
+            if (scanner.hasNextInt()) {
+                int choose = scanner.nextInt();
+                if (choose == 1) {
+                    System.out.println("Area rectangle = " + rectangle.areaCalculator(1,2));
+                } else if (choose == 2){
+                    System.out.println("Perimeter rectangle = " + rectangle.perimeterCalculator(1,2));
+                } else {
+                    System.out.println("Negative number, please repeat: ");
+                }
+            }
     }
 }
--- src/essential_homework1/task2/Rectangle.java
+++ src/essential_homework1/task2/Rectangle.java
@@ -13,7 +13,7 @@ public class Rectangle {
         while (true)
             if (scanner.hasNextDouble()) {
                 side1 = scanner.nextDouble();
-                if (side1 < 0) {
+                if (side1 < 0 && side1 == 0) {
                     System.out.println("Negative number, please repeat: ");
                 } else {
                     break;
@@ -27,7 +27,7 @@ public class Rectangle {
         while (true)
             if (scanner.hasNextDouble()) {
                 side2 = scanner.nextDouble();
-                if (side2 < 0) {
+                if (side2 < 0 && side2 == 0) {
                     System.out.println("Negative number, please repeat: ");
                 } else {
                     break;
@@ -39,5 +39,40 @@ public class Rectangle {
 
         double area = side1 * side2;
         return area;
+    }
+
+    double perimeterCalculator(double side1, double side2) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the width of the rectangle: ");
+        while (true)
+            if (scanner.hasNextDouble()) {
+                side1 = scanner.nextDouble();
+                if (side1 < 0 || side1 == 0) {
+                    System.out.println("Negative number, please repeat: ");
+                } else {
+                    break;
+                }
+            } else {
+                System.out.println("Invalid value entered, please repeat: ");
+                scanner.hasNextLine();
+            }
+
+        System.out.println("Enter the length of the rectangle: ");
+        while (true)
+            if (scanner.hasNextDouble()) {
+                side2 = scanner.nextDouble();
+                if (side2 < 0 || side2 == 0 || side2 == side1) {
+                    System.out.println("Negative number, please repeat: ");
+                } else {
+                    break;
+                }
+            } else {
+                System.out.println("Invalid value entered, please repeat");
+                scanner.hasNextLine();
+            }
+
+        double perimeter = 2 * (side1 + side2);
+        return perimeter;
     }
 }
